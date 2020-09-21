8afb2ea1de1f356eb40dde5c33d1900324b42e29
HansaeJu <sparky@hanyang.ac.kr>
2020-09-21 14:46:32 +0900

    permission changed: tabs to activeTab

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   save.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { greeting: "save" }, function (response) {
-        console.log(response.farewell)
+        console.log("responded")
         showTable()
       })
       
